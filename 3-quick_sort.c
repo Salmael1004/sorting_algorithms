58d62c60d96c0557694314fa1a64986468d0e6b9
Salma <salmaelaadam@gmail.com>
2023-07-24 12:12:50 +0100

    3. Quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,45 +10,45 @@ void quick_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	quick_recursion(array, 0, size - 1, size);
+	q_recu(array, 0, size - 1, size);
 }
 
 /**
- * quick_recursion - helper function for Quicksort
+ * q_recu - helper function for Quicksort
  * @array: array to sort
  * @low: index of the leftmost element
  * @high: index of the rightmost element
  * @size: size of the array
  */
-void quick_recursion(int *array, int low, int high, size_t size)
+void q_recu(int *array, int low, int high, size_t size)
 {
 	if (low < high)
 	{
-		int pivot = partition(array, low, high, size);
+		int zb1 = q_part(array, low, high, size);
 
-		quick_recursion(array, low, pivot - 1, size);
-		quick_recursion(array, pivot + 1, high, size);
+		q_recu(array, low, zb1 - 1, size);
+		q_recu(array, zb1 + 1, high, size);
 	}
 }
 
 /**
- * partition - finds a pivot index for Quicksort
+ * q_part - finds a pivot index for Quicksort
  * @array: array to find the pivot in
  * @low: index of the leftmost element
  * @high: index of the rightmost element
  * @size: size of the array
  *
- * Return: the index of the pivot element
+ * Return: the index of the zb1 element
  */
-int partition(int *array, int low, int high, size_t size)
+int q_part(int *array, int low, int high, size_t size)
 {
-	int pivot = array[high];
+	int zb1 = array[high];
 	int i = low - 1;
 	int j;
 
 	for (j = low; j < high; j++)
 	{
-		if (array[j] <= pivot)
+		if (array[j] <= zb1)
 		{
 			i++;
 			if (i != j)
