00b1092adddab002ae6a7d1e40224587c12096ca
Salma <salmaelaadam@gmail.com>
2023-07-24 12:27:33 +0100

    update header

--- sort.h
+++ sort.h
@@ -28,6 +28,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void q_recu(int *array, int left, int right, size_t size);
 int q_part(int *array, int left, int right, size_t size);
+void shell_sort(int *array, size_t size);
 
 
 
